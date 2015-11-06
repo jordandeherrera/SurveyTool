6d2186543a0c059c905ddfa5edf65e18526365b8
jsdeherrera <jordan.deherrera@gmail.com>
2015-11-06 15:18:24 -0500

    Added a script to append a contact list and automatically generate validation codes for the contact list.

---
+++ AppendContactListToValidationCodes.R
@@ -0,0 +1,20 @@
+# Import the contacts to add
+Contacts <- read.csv("Contacts.csv")
+
+# Load the random library in order to generate survey validation codes
+library(random)
+
+# Set the x variable equal to the length of the new contacts to add
+x <- length(Contacts[,1])
+
+# Create a new data frame to add to Validation.Code
+Validation.Code.New <- data.frame(Code = randomStrings(n=x, len=5), Count = rep(0,x), Name = Contacts$Name, Title = Contacts$Title, Organization = Contacts$Organization, ResponseNo = rep(0,x))
+
+# Change first column name to append it
+colnames(Validation.Code.New)[1] <- "Code"
+
+# Add rows to Validation.Code
+Validation.Code <- rbind(Validation.Code, Validation.Code.New)
+
+# Save the results
+save(Validation.Code, file="Validation.Rdata")
