12947b6f73a6f78314457323d7d8e50a8538dced
jsdeherrera <jordan.deherrera@gmail.com>
2015-11-08 20:04:39 -0500

    Added a function to paste e-mail string into validation.code data frame.

--- CreateBaseValidationDataFrame.R
+++ CreateBaseValidationDataFrame.R
@@ -8,7 +8,7 @@ library(random)
 x <- length(Contacts[,1])
 
 # Create a new data frame to add to Validation.Code
-Validation.Code <- data.frame(Code = randomStrings(n=x, len=5), Count = rep(0,x), Name = Contacts$Name, Title = Contacts$Title, Organization = Contacts$Organization, ResponseNo = rep(0,x), Email = rep(as.character(""),x), Contact = rep(0,x))
+Validation.Code <- data.frame(Code = randomStrings(n=x, len=5), Count = rep(0,x), Name = Contacts$Name, Title = Contacts$Title, Organization = Contacts$Organization, ResponseNo = rep(0,x), Email = rep("blank@email.com",x), Contact = rep(0,x))
 
 # Change first column name to append it
 colnames(Validation.Code)[1] <- "Code"
