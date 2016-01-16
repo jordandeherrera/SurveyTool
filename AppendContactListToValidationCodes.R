e78a97b27ce06a267691c44edd9994c010a66f07
jsdeherrera <jordan.deherrera@gmail.com>
2016-01-16 00:03:58 -0500

    Updated for newest contact list

--- AppendContactListToValidationCodes.R
+++ AppendContactListToValidationCodes.R
@@ -8,7 +8,7 @@ library(random)
 x <- length(Contacts[,1])
 
 # Create a new data frame to add to Validation.Code
-Validation.Code.New <- data.frame(Code = randomStrings(n=x, len=5), Count = rep(0,x), Name = Contacts$Name, Title = Contacts$Title, Organization = Contacts$Organization, ResponseNo = rep(0,x))
+Validation.Code.New <- data.frame(Code = randomStrings(n=x, len=5), Count = rep(0,x), Name = Contacts$Name, Title = Contacts$Title, Organization = Contacts$Organization, ResponseNo = rep(0,x), Email = rep(NA,x), Contact = rep(0,x))
 
 # Change first column name to append it
 colnames(Validation.Code.New)[1] <- "Code"
